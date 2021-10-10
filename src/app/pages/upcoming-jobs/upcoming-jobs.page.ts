3f80fc51932f6220b094987dd241bbf8e1437730
Deepak Bawa <deepak4bawa@gmail.com>
2021-10-10 13:48:54 +0530

    added pages

--- src/app/pages/upcoming-jobs/upcoming-jobs.page.ts
+++ src/app/pages/upcoming-jobs/upcoming-jobs.page.ts
@@ -1,17 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoaderService } from 'src/app/custom/services/loader.service';
+import { ToasterService } from 'src/app/custom/services/toaster.service';
+import { ArticlesService } from 'src/app/services/articles.service';
 
 @Component({
-  selector: 'app-upcoming-jobs',
-  templateUrl: './upcoming-jobs.page.html',
-  styleUrls: ['./upcoming-jobs.page.scss'],
+	selector: 'app-upcoming-jobs',
+	templateUrl: './upcoming-jobs.page.html',
+	styleUrls: ['./upcoming-jobs.page.scss'],
 })
 export class UpcomingJobsPage implements OnInit {
 
-    title='Upcoming Jobs';
-    
-  constructor() { }
+	title= 'Articles Listing';
+	page= 'Upcoming Jobs';
+	articles;
+	isArticlesFound = false;
+	loading;
+    completed = false;
 
-  ngOnInit() {
-  }
+	constructor(
+		protected articlesService: ArticlesService,
+		protected activatedRoute: ActivatedRoute, 
+		protected router: Router,
+        protected toasterService: ToasterService,
+        protected loaderService: LoaderService
+	) {}
 
+	ngOnInit() {
+		this.getAllArticles();
+	}
+
+	async getAllArticles() {
+
+		await this.loaderService.open();
+		this.articlesService.getAllArticles().subscribe(
+			async (result) => {
+				this.articles = result?.data?.article;
+				
+				for (let index = 0; index < 11; index++) {
+					this.articles.push(this.articles[0]);					
+				}
+				this.isArticlesFound = true;
+			},
+			async (ex) => {
+				console.log('ex', ex);
+                this.toasterService.presentToast(ex.message);
+			},
+			async () => {
+                this.completed = true;
+				if( this.isArticlesFound ) {
+                    await this.loaderService.close();
+
+				}
+			}
+		);
+	}
+
+	identify( index, item ) {
+		return item._id;
+	}
+
+	goToDescription(articleId) {
+		console.log('articleId', articleId);
+		this.router.navigate([`article-details/${articleId}`]);
+	}
 }
